6a625643b712b7d3fecc65d21d79f8aec4b5540a
ZeroXClem <131801011+ZeroXClem@users.noreply.github.com>
2023-07-17 16:35:18 -0400

    Update notion-hugo.config.ts
    Added correct page url.

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://fragrant-macaroni-678.notion.site/Notion-DoIt-8ba64f9163bf41d3863676566f8df051?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
